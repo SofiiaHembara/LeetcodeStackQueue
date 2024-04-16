13cbecfa098735c817bc6ce43b68b099e0a08f26
Sofiia Hembara <hembara.pn@ucu.edu.ua>
2024-04-16 12:26:27 +0300

    Change a solution

--- Implement Queue using Stacks/answer1.py
+++ Implement Queue using Stacks/answer1.py
@@ -1,22 +1,48 @@
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+class Stack:
+    def __init__(self):
+        self.head = None
+
+    def push(self, item):
+        self.head = Node(item, self.head)
+
+    def pop(self):
+        if self.head is None:
+            return None
+        value = self.head.value
+        self.head = self.head.next
+        return value
+
+    def peek(self):
+        return self.head.value if self.head else None
+
+    def is_empty(self):
+        return self.head is None
+
+
 class MyQueue:
     def __init__(self) -> None:
-        self.stack1 = []
-        self.stack2 = []
+        self.stack1 = Stack()
+        self.stack2 = Stack()
 
     def push(self, x:int) -> None:
-        self.stack1.append(x)
+        self.stack1.push(x)
     
     def pop(self) -> int:
-        if not self.stack2:
-            while self.stack1:
-                self.stack2.append(self.stack1.pop())
+        if self.stack2.is_empty():
+            while not self.stack1.is_empty():
+                self.stack2.push(self.stack1.pop())
         return self.stack2.pop()
     
     def peek(self) -> int:
-        if not self.stack2:
-            while self.stack1:
-                self.stack2.append(self.stack1.pop())
-        return self.stack2[-1]
+        if self.stack2.is_empty():
+            while not self.stack1.is_empty():
+                self.stack2.push(self.stack1.pop())
+        return self.stack2.peek()
     
     def empty(self) -> bool:
-        return len(self.stack1) == 0 and len(self.stack2) == 0
+        return self.stack1.is_empty() and self.stack2.is_empty()
