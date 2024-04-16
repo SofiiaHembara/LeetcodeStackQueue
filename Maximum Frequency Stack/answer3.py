c0e2295f418eedf5f83fac0fb9e017f7b7f320ab
Sofiia Hembara <hembara.pn@ucu.edu.ua>
2024-04-16 22:58:16 +0300

    change the solution

--- Maximum Frequency Stack/answer3.py
+++ Maximum Frequency Stack/answer3.py
@@ -1,21 +1,52 @@
-class FreqStack:
+from collections import defaultdict
 
+class Node:
+    def __init__(self, value=None, next=None):
+        self.value = value
+        self.next = next
+
+class LinkedList:
     def __init__(self):
-        self.count = {}
-        self.max_count = 0
-        self.stack = {}
+        self.head = None
 
-    def push(self, val: int) -> None:
-        value_count = 1 + self.count.get(val, 0)
-        self.count[val] = value_count
-        if value_count > self.max_count:
-            self.max_count = value_count
-            self.stack[value_count] = []
-        self.stack[value_count].append(val)
+    def append(self, value):
+        if not self.head:
+            self.head = Node(value)
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = Node(value)
+
+    def remove(self, node_to_remove):
+        if not self.head:
+            return
+        if self.head == node_to_remove:
+            self.head = self.head.next
+            return
+        current = self.head
+        while current.next and current.next != node_to_remove:
+            current = current.next
+        if current.next:
+            current.next = current.next.next
+
+class FreqStack:
+    def __init__(self):
+        self.freq = defaultdict(int)
+        self.group = defaultdict(list)
+        self.max_freq = 0
+        self.all_elements = LinkedList()
+
+    def push(self, x: int) -> None:
+        self.freq[x] += 1
+        if self.freq[x] > self.max_freq:
+            self.max_freq = self.freq[x]
+        self.group[self.freq[x]].append(x)
+        self.all_elements.append(x)
 
     def pop(self) -> int:
-        res = self.stack[self.max_count].pop()
-        self.count[res] -= 1
-        if not self.stack[self.max_count]:
-            self.max_count -= 1
-        return res 
+        x = self.group[self.max_freq].pop()
+        self.freq[x] -= 1
+        if not self.group[self.max_freq]:
+            self.max_freq -= 1
+        return x
